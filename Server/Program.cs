5cd227cefd6e6e5f49e30f2bebd670c978b20990
Henrik Bulskov <bulskov@ruc.dk>
2022-10-20 13:23:39 +0200

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using Server;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var server = new TcpListener(IPAddress.Loopback, 5000);
+server.Start();
+Console.WriteLine("Server started...");
+
+while (true)
+{
+    var client = server.AcceptTcpClient();
+    Console.WriteLine("Client connected...");
+
+    try
+    {
+        HandleClient(client);
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Unable to communicate with client...");
+    }
+
+}
+
+static void HandleClient(TcpClient client)
+{
+    var stream = client.GetStream();
+
+    var buffer = new byte[1024];
+
+    var rcnt = stream.Read(buffer);
+
+    var requestText = Encoding.UTF8.GetString(buffer, 0, rcnt);
+    Console.WriteLine(requestText);
+
+    var request = JsonSerializer.Deserialize<Request>(requestText);
+
+    if (string.IsNullOrEmpty(request?.Method))
+    {
+        Response response = CreateReponse("4 Missing method");
+        SendResponse(stream, response);
+    }
+    stream.Close();
+}
+
+
+
+static void SendResponse(NetworkStream stream, Response response)
+{
+    var responseText = JsonSerializer.Serialize<Response>(response);
+    var responseBuffer = Encoding.UTF8.GetBytes(responseText);
+    stream.Write(responseBuffer);
+}
+
+static Response CreateReponse(string status, string body = "")
+{
+    return new Response
+    {
+        Status = status,
+        Body = body
+    };
+}
+
